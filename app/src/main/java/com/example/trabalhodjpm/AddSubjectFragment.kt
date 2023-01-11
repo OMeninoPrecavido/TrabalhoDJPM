4e6902fe278b9157874077a0a072fcd12fb14be2
OMeninoPrecavido <andrecoccoconde20@gmail.com>
2023-01-11 23:39:13 +0000

    Adição de Subject novo ao Firebase implementado.
    Adição à Controller list e posteriormente ao firebase implementados.

--- app/src/main/java/com/example/trabalhodjpm/AddSubjectFragment.kt
+++ app/src/main/java/com/example/trabalhodjpm/AddSubjectFragment.kt
@@ -5,9 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class AddSubjectFragment : DialogFragment() {
+
+    private lateinit var firebaseAuth : FirebaseAuth
+    private lateinit var database : DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +29,25 @@ class AddSubjectFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         val btnCreate = view.findViewById<Button>(R.id.create_button)
         val btnCancel = view.findViewById<Button>(R.id.cancel_button)
 
         btnCreate.setOnClickListener {
+            val shortRest = 5
+            val longRest = 15
+            val studyTime = 25
+            val name = view.findViewById<EditText>(R.id.new_subject_name).text
+
+            database = FirebaseDatabase.getInstance().getReference("Users")
+
+            Controller.Companion.currUserSubjList.add(Subject(name.toString(), shortRest, longRest, studyTime, 0))
+
+            val id = firebaseAuth.currentUser?.uid.toString()
+
+            database.child(id).child("subjectList").setValue(Controller.Companion.currUserSubjList)
+            
             dismiss()
         }
 
